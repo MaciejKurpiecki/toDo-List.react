f0b4331a2582f9352bd575e18be783de3bfe2786
Maciej Kurpiecki <recon4173@wp.pl>
2023-01-27 14:11:05 +0100

    Tasks deconstructed

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,21 +1,21 @@
 import './style.css';
 
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="list">
-        {props.tasks.map(task => (
+        {tasks.map(task => (
             <li key={task.id}
-                className = {
-                    `list__item${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`
-            }>
+                className={
+                    `list__item${task.done && hideDoneTasks ? " list__item--hidden" : ""}`
+                }>
                 <button className="list__doneButton">
-            {task.done ? "✔" : ""}
-            </button> 
-            <button className="list__removeButton">
-            🗑
-            </button>          
-            <span className= {`list__task${task.done ? " list__item--done" : ""}`}>
-            {task.content}
-            </span>
+                    {task.done ? "✔" : ""}
+                </button>
+                <button className="list__removeButton">
+                    🗑
+                </button>
+                <span className={`list__task${task.done ? " list__item--done" : ""}`}>
+                    {task.content}
+                </span>
             </li>
         ))}
     </ul>
