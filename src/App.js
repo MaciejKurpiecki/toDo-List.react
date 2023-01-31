173d75f3730a963c922b444b0c021b82d12a79b0
Maciej Kurpiecki <recon4173@wp.pl>
2023-01-31 18:10:36 +0100

    Hide done tasks function added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Tasks from './Tasks';
 import Buttons from './Buttons';
 import Section from './Section';
 import Container from './Container';
+import { useState } from 'react';
 
 const tasks = [
   { id: 1, content: "przejsc na react", done: false },
   { id: 2, content: "zjeść obiad", done: true },
 ];
 
-const hideDoneTasks = false;
-
 function App() {
+  const [hideDoneTasks, setHideDoneTasks] = useState(false);
+
+  const toggleHideDoneTasks = () => {
+    setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
+  };
   return (
     <Container>
       <header>
@@ -24,7 +28,11 @@ function App() {
           <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
         }
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+          <Buttons
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            toggleHideDoneTasks={toggleHideDoneTasks}
+          />
         } />
     </Container>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,12 +1,14 @@
 import './style.css';
 
-const Buttons = ({ tasks, hideDoneTasks }) => {
+const Buttons = ({ tasks, hideDoneTasks, toggleHideDoneTasks }) => {
     if (!tasks.length) {
         return null;
     }
     return (
         <div className="allTasksButtons">
-            <button className="button">
+            <button
+                onClick={toggleHideDoneTasks}
+                className="button">
                 {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button
