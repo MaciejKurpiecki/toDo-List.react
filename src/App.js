3c099b9b3846475cc5b3aa111a4413df8cb22dc9
Maciej Kurpiecki <recon4173@wp.pl>
2023-02-01 16:33:56 +0100

    tasks array cleared

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import { useState } from 'react';
 
 function App() {
   const [hideDoneTasks, setHideDoneTasks] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejsc na react", done: false },
-    { id: 2, content: "zjeść obiad", done: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDoneTasks = () => {
     setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
