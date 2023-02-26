cd518b0b3422c118629f030aff4307a877ac6122
Maciej Kurpiecki <recon4173@wp.pl>
2023-02-26 18:17:22 +0100

    added global style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
 }
@@ -17,3 +20,4 @@ body {
   padding: 20px;
   block-size: auto;
 }
+`;
