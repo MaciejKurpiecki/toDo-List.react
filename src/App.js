b11c20174084dd168e234496397a13e65d2bb597
Maciej Kurpiecki <recon4173@wp.pl>
2023-04-25 17:13:16 +0200

    adding react router wip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import TasksApp from "./features/tasks/TasksApp";
 
-export default () => <TasksApp />;
+export default () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">
+            Zadania
+          </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <TasksApp />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
