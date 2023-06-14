0cfa202f21b191f47caa7c6141d85a8bb0253a48
Maciej Kurpiecki <recon4173@wp.pl>
2023-06-14 18:57:08 +0200

    trim

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getExampleTasks = async () => {
     const response = await axios.get("/toDo-List.react/exampleTasks.json");
+    
     return response.data;
 };
